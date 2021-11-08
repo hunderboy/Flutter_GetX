946df942ee3f59451630eeaba0ba0fd918cb80fd
Dev_dominos <masury2011@gmail.com>
2021-11-09 01:56:36 +0900

    once, ever , debounce, interval 설정

--- lib/src/controller/count_controller_with_reactive.dart
+++ lib/src/controller/count_controller_with_reactive.dart
@@ -20,9 +20,11 @@ class CountControllerWithReactive extends GetxController {
   // 반응형 트리거 사용
   @override
   void onInit() {
-    // worker, ever , debounce
-
-    ever(count, (_)=>print("매번호출!!")); // count(데이터)가 바뀔때마다 매번 호출 , reactive 상태(RxInt,RxString,, 등등)일때만 가능
+    // once, ever , debounce, interval
+    // ever(count, (_)=>print("매번호출!!"));    // count(데이터)가 바뀔때마다 매번 호출 , reactive 상태(RxInt,RxString,, 등등)일때만 가능
+    // once(count, (_)=>print("한번만 호출!!"));  // 한번만 호출되고, 다시 안불려짐
+    // debounce(count, (_)=>print("마지막 변경에 한번만 호출!!"),time: Duration(seconds: 1)); // 검색 기능 작성, 서버전달할때 사용자가 입력을 마치고 할때 사용
+    interval(count, (_)=>print("변경되고 있는 동안 1초마다 호출!!"),time: Duration(seconds: 1));
     super.onInit();
   }
 
